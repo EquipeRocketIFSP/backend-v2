28dd7ae0717ba1b252d5e87a1c7985b6147c4cd9
hshima <22624182+hshima@users.noreply.github.com>
2023-03-21 00:44:57 -0300

    aplica alteração no endpoint prontuario

--- src/main/java/br/vet/certvet/controllers/ProntuarioController.java
+++ src/main/java/br/vet/certvet/controllers/ProntuarioController.java
@@ -1,8 +1,11 @@
 package br.vet.certvet.controllers;
 
+import br.vet.certvet.dto.ProntuarioRequest;
+import br.vet.certvet.models.Documento;
 import br.vet.certvet.models.Prontuario;
 import br.vet.certvet.services.PdfService;
 import br.vet.certvet.services.ProntuarioService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -10,10 +13,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/prontuario")
+@Slf4j
 public class ProntuarioController extends BaseController {
 
     @Autowired
@@ -42,6 +47,36 @@ public class ProntuarioController extends BaseController {
         return prontuario.isPresent()
                 ? ResponseEntity.ok().contentType(MediaType.APPLICATION_PDF).body(pdfService.retrieveFromRepository(prontuario.get()))
                 : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Prontuario> create(
+            @RequestBody ProntuarioRequest prontuarioDto
+            ){
+
+        Prontuario p = null;
+        try{
+            p = prontuarioDto.convert();
+        } catch (RuntimeException e){
+            throwExceptionFromController(e);
+        }
+        Optional<Prontuario> saved = prontuarioService.createProntuario(p);
+
+        return saved.map(
+                prontuario -> ResponseEntity.created(
+                        URI.create(prontuario.getCodigo())
+                        ).body(prontuario))
+                .orElseGet(
+                        () -> ResponseEntity.internalServerError().build()
+                );
+    }
+
+    @PostMapping("/{prontuarioId}")
+    public ResponseEntity<Documento> addDocument(
+            @PathVariable Long prontuarioId,
+            @RequestBody ProntuarioRequest prontuarioDto
+    ){
+        return null;
     }
 
 
