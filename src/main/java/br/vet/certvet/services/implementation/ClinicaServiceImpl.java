74579d6ed4bc44655377fdbe8cf7eec084913fcc
Caique <caiquedaniel8@gmail.com>
2023-04-08 21:25:00 -0300

    Enviar email com código da clínica cadastrada.

--- src/main/java/br/vet/certvet/services/implementation/ClinicaServiceImpl.java
+++ src/main/java/br/vet/certvet/services/implementation/ClinicaServiceImpl.java
@@ -4,13 +4,16 @@ import br.vet.certvet.dto.requests.ClinicaInicialRequestDto;
 import br.vet.certvet.dto.requests.ClinicaRequestDto;
 import br.vet.certvet.dto.requests.FuncionarioRequestDto;
 import br.vet.certvet.dto.requests.VeterinarioRequestDto;
+import br.vet.certvet.exceptions.BadGatewayException;
 import br.vet.certvet.exceptions.ConflictException;
 import br.vet.certvet.exceptions.NotFoundException;
 import br.vet.certvet.models.Clinica;
 import br.vet.certvet.models.Usuario;
 import br.vet.certvet.repositories.ClinicaRepository;
 import br.vet.certvet.services.ClinicaService;
+import br.vet.certvet.services.EmailService;
 import br.vet.certvet.services.UsuarioService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +22,7 @@ import java.sql.SQLException;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class ClinicaServiceImpl implements ClinicaService {
 
     @Autowired
@@ -26,6 +30,9 @@ public class ClinicaServiceImpl implements ClinicaService {
 
     @Autowired
     private UsuarioService usuarioService;
+
+    @Autowired
+    private EmailService emailService;
 
     @Override
     @Transactional(rollbackFor = {SQLException.class, RuntimeException.class})
@@ -35,14 +42,17 @@ public class ClinicaServiceImpl implements ClinicaService {
         if (response.isPresent())
             throw new ConflictException("Clínica já existe.");
 
+        Usuario usuario;
         Clinica clinica = this.clinicaRepository.saveAndFlush(new Clinica(dto));
 
         if (dto.dono_crmv != null && !dto.dono_crmv.isEmpty()) {
-            Usuario usuario = this.usuarioService.create(ClinicaServiceImpl.getDonoResponsavelTecnicoDto(dto), clinica);
+            usuario = this.usuarioService.create(ClinicaServiceImpl.getDonoResponsavelTecnicoDto(dto), clinica);
 
             clinica.setResponsavelTecnico(usuario);
             this.clinicaRepository.saveAndFlush(clinica);
-        } else this.usuarioService.create(ClinicaServiceImpl.getDonoDto(dto), clinica);
+        } else usuario = this.usuarioService.create(ClinicaServiceImpl.getDonoDto(dto), clinica);
+
+        this.sendCodigoClinicaThoughEmail(clinica, usuario);
 
         return clinica;
     }
@@ -82,6 +92,22 @@ public class ClinicaServiceImpl implements ClinicaService {
         clinica.fill(dto);
 
         return this.clinicaRepository.saveAndFlush(clinica);
+    }
+
+    private void sendCodigoClinicaThoughEmail(Clinica clinica, Usuario to) {
+        final String subject = "Código da Clínica - CertVet";
+        final String message = "<h1>Redefinir senha - CertVet</h1>" +
+                "<br/>" +
+                "<p>Clínica cadastrada com sucesso.</p>" +
+                "<p>O código de acesso da Clínica é: " + clinica.getCode() + "</p>";
+
+        try {
+            this.emailService.sendTextMessage(to.getUsername(), subject, message);
+        } catch (Exception e) {
+            ClinicaServiceImpl.log.error(e.getMessage());
+
+            throw new BadGatewayException("Não foi possivel enviar o e-mail com o código da clínica");
+        }
     }
 
     private static FuncionarioRequestDto getDonoDto(ClinicaInicialRequestDto dto) {
